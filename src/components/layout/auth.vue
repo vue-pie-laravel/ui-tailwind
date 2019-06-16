94451b438d7fa813c2c1876d0b9dc58f357ed94a
Marc Newton <php@marcnewton.info>
2019-06-16 22:32:41 +0100

    Create auth.vue

---
+++ src/components/layout/auth.vue
@@ -0,0 +1,13 @@
+<template>
+
+    <div class="min-h-screen flex justify-center items-center layout-auth">
+    
+        TODO : LOGIN SCREEN HERE
+
+        TODO : RECOVERY SCREEN HERE
+
+        TODO : LOGIN SCREEN HERE
+
+    </div>
+    
+</template>
